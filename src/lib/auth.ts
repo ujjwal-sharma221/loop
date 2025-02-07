a6978ac7ff6236c846a6d3aa382f1e3dbdd2835d
Ujjwal Sharma <ujjwalshrm2@gmail.com>
2025-02-08 00:42:53 +0530

    db

---
+++ src/lib/auth.ts
@@ -0,0 +1,15 @@
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { betterAuth } from "better-auth";
+
+import { db } from "@/db";
+import * as schema from "@/db/schema";
+
+export const auth = betterAuth({
+  emailAndPassword: {
+    enabled: true,
+  },
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    schema,
+  }),
+});
