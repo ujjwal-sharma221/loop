23209045ee6e5dfdcea487528ea163dfe7336621
Ujjwal Sharma <ujjwalshrm2@gmail.com>
2025-02-12 01:33:41 +0530

    carousel

---
+++ src/components/filter-carousel.tsx
@@ -0,0 +1,107 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import { cn } from "@/lib/utils";
+import { Badge } from "./ui/badge";
+import {
+  Carousel,
+  CarouselApi,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { Skeleton } from "./ui/skeleton";
+
+interface FilterCarouselProps {
+  value?: string | null;
+  isLoading?: boolean;
+  onSelect: (value: string | null) => void;
+  data: {
+    value: string;
+    label: string;
+  }[];
+}
+
+export function FilterCarousel({
+  value,
+  onSelect,
+  data,
+  isLoading,
+}: FilterCarouselProps) {
+  const [api, setApi] = useState<CarouselApi>();
+  const [current, setCurrent] = useState(0);
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    if (!api) return;
+
+    setCount(api.scrollSnapList().length);
+    setCurrent(api.selectedScrollSnap() + 1);
+
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap() + 1);
+    });
+  }, [api]);
+
+  return (
+    <div className="relative w-full">
+      <div
+        className={cn(
+          "pointer-events-none absolute left-12 z-10 h-full w-20 bg-gradient-to-r from-white to-transparent",
+          current === 1 && "hidden",
+        )}
+      ></div>
+
+      <Carousel
+        setApi={setApi}
+        opts={{
+          align: "start",
+          dragFree: true,
+        }}
+        className="w-full px-12"
+      >
+        <CarouselContent className="-ml-3">
+          {!isLoading && (
+            <CarouselItem
+              className="basis-auto pl-3"
+              onClick={() => onSelect(null)}
+            >
+              <Badge
+                variant={!value ? "default" : "secondary"}
+                className="cursor-pointer whitespace-nowrap rounded-lg px-3 py-1 text-sm"
+              >
+                All
+              </Badge>
+            </CarouselItem>
+          )}
+          {isLoading &&
+            Array.from({ length: 13 }).map((_, i) => (
+              <CarouselItem key={i}>
+                <Skeleton className="h-full w-[100px] rounded-lg px-3 py-1 text-sm font-semibold" />
+              </CarouselItem>
+            ))}
+
+          {!isLoading &&
+            data.map((item) => (
+              <CarouselItem
+                className="basis-auto pl-3"
+                key={item.value}
+                onClick={() => onSelect(item.value)}
+              >
+                <Badge
+                  variant={value === "null" ? "default" : "secondary"}
+                  className="cursor-pointer whitespace-nowrap rounded-lg px-3 py-1 text-sm"
+                >
+                  {item.label}
+                </Badge>
+              </CarouselItem>
+            ))}
+        </CarouselContent>
+        <CarouselPrevious className="left-0 z-20" />
+        <CarouselNext className="right-0 z-20" />
+      </Carousel>
+    </div>
+  );
+}
--- src/modules/home/ui/category-section.tsx
+++ src/modules/home/ui/category-section.tsx
@@ -1,12 +1,43 @@
 "use client";
 
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import { LoaderCircle } from "lucide-react";
+
 import { trpc } from "@/trpc/client";
+import { FilterCarousel } from "@/components/filter-carousel";
 
 interface CategoriesSectionProps {
   categoryId?: string;
 }
 
 export function CategoriesSection({ categoryId }: CategoriesSectionProps) {
+  return (
+    <Suspense
+      fallback={
+        <div className="flex items-center justify-center text-muted-foreground">
+          <LoaderCircle className="animate-spin" />
+        </div>
+      }
+    >
+      <ErrorBoundary fallback={<p className="text-destructive">Error...</p>}>
+        <CategoriesSectionSuspense categoryId={categoryId} />
+      </ErrorBoundary>
+    </Suspense>
+  );
+}
+
+function CategoriesSectionSuspense({ categoryId }: CategoriesSectionProps) {
   const [categories] = trpc.catgories.getMany.useSuspenseQuery();
-  return <div>{JSON.stringify(categories)}</div>;
+  const data = categories.map((category) => ({
+    value: category.id,
+    label: category.name,
+  }));
+  return (
+    <FilterCarousel
+      onSelect={(x) => console.log(x)}
+      value={categoryId}
+      data={data}
+    ></FilterCarousel>
+  );
 }
